6e97f7c119bec4bf56830561418829b8792ea20d
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-06-21 00:32:15 +0300

    Adding components. Stage 3.

--- index.php
+++ index.php
@@ -2,7 +2,68 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("HomeSpace — Colorlib Website Template"); ?>
     <div class="slide-one-item home-slider owl-carousel">
-
+        <? /*
+        $GLOBALS["arrFilter"] = array(
+            "IBLOCK_ID" => 5,
+            "BOOL_PRIORITY" => "Y"
+        ); */ ?>
+        <? /* $APPLICATION->IncludeComponent(
+            "bitrix:news.list",
+            "",
+            Array(
+                "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                "ADD_SECTIONS_CHAIN" => "Y",
+                "AJAX_MODE" => "N",
+                "AJAX_OPTION_ADDITIONAL" => "",
+                "AJAX_OPTION_HISTORY" => "N",
+                "AJAX_OPTION_JUMP" => "N",
+                "AJAX_OPTION_STYLE" => "Y",
+                "CACHE_FILTER" => "N",
+                "CACHE_GROUPS" => "Y",
+                "CACHE_TIME" => "36000000",
+                "CACHE_TYPE" => "A",
+                "CHECK_DATES" => "Y",
+                "DETAIL_URL" => "",
+                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_DATE" => "Y",
+                "DISPLAY_NAME" => "Y",
+                "DISPLAY_PICTURE" => "Y",
+                "DISPLAY_PREVIEW_TEXT" => "Y",
+                "DISPLAY_TOP_PAGER" => "N",
+                "FIELD_CODE" => array("",""),
+                "FILTER_NAME" => "arrFilter",
+                "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                "IBLOCK_ID" => $_REQUEST["ID"],
+                "IBLOCK_TYPE" => "ads",
+                "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                "INCLUDE_SUBSECTIONS" => "Y",
+                "MESSAGE_404" => "",
+                "NEWS_COUNT" => "20",
+                "PAGER_BASE_LINK_ENABLE" => "N",
+                "PAGER_DESC_NUMBERING" => "N",
+                "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                "PAGER_SHOW_ALL" => "N",
+                "PAGER_SHOW_ALWAYS" => "N",
+                "PAGER_TEMPLATE" => ".default",
+                "PAGER_TITLE" => "Новости",
+                "PARENT_SECTION" => "",
+                "PARENT_SECTION_CODE" => "",
+                "PREVIEW_TRUNCATE_LEN" => "",
+                "PROPERTY_CODE" => array("",""),
+                "SET_BROWSER_TITLE" => "Y",
+                "SET_LAST_MODIFIED" => "N",
+                "SET_META_DESCRIPTION" => "Y",
+                "SET_META_KEYWORDS" => "Y",
+                "SET_STATUS_404" => "N",
+                "SET_TITLE" => "Y",
+                "SHOW_404" => "N",
+                "SORT_BY1" => "ACTIVE_FROM",
+                "SORT_BY2" => "SORT",
+                "SORT_ORDER1" => "DESC",
+                "SORT_ORDER2" => "ASC",
+                "STRICT_SECTION_CHECK" => "N"
+            )
+        ); */ ?>
         <div class="site-blocks-cover" style="background-image: url(<?= SITE_TEMPLATE_PATH; ?>/images/hero_bg_1.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
 
             <div class="text">
@@ -294,6 +355,25 @@ $APPLICATION->SetTitle("HomeSpace — Colorlib Website Template"); ?>
                     </div>
                 </div>
             </div>
+            <? /* $APPLICATION->IncludeComponent(
+                "bitrix:news.line",
+                "",
+                Array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "300",
+                    "CACHE_TYPE" => "A",
+                    "DETAIL_URL" => "#SITE_DIR#/services/#ELEMENT_CODE#",
+                    "FIELD_CODE" => array("", ""),
+                    "IBLOCKS" => array(),
+                    "IBLOCK_TYPE" => "services",
+                    "NEWS_COUNT" => "20",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC"
+                )
+            ); */ ?>
             <div class="row">
                 <div class="col-md-6 col-lg-4 mb-4">
                     <a href="#" class="service text-center border rounded"> <span class="icon flaticon-house"></span>
@@ -417,6 +497,25 @@ $APPLICATION->SetTitle("HomeSpace — Colorlib Website Template"); ?>
                     </div>
                 </div>
             </div>
+            <? /* $APPLICATION->IncludeComponent(
+                "bitrix:news.line",
+                "",
+                Array(
+                    "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                    "CACHE_GROUPS" => "Y",
+                    "CACHE_TIME" => "300",
+                    "CACHE_TYPE" => "A",
+                    "DETAIL_URL" => "",
+                    "FIELD_CODE" => array("",""),
+                    "IBLOCKS" => array(),
+                    "IBLOCK_TYPE" => "agents",
+                    "NEWS_COUNT" => "20",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_ORDER2" => "ASC"
+                )
+            ); */ ?>
             <div class="row block-13">
                 <div class="nonloop-block-13 owl-carousel">
                     <div class="slide-item">
