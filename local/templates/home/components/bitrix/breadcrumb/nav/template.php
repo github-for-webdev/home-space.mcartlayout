0806b7e0e726c0545a7222353507bfe287779f87
Andrey Kuznetsov <steamprofile2020@gmail.com>
2022-06-27 22:06:13 +0300

    Breadcrumb. Stage 2.

--- local/templates/home/components/bitrix/breadcrumb/nav/template.php
+++ local/templates/home/components/bitrix/breadcrumb/nav/template.php
@@ -1,46 +1,33 @@
-<? if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 global $APPLICATION;
-
 if (empty($arResult)) return "";
-
-$strReturn = '';
-
-$css = $APPLICATION->GetCSSArray();
-if (!is_array($css) || !in_array("/bitrix/css/main/font-awesome.css", $css))
-{
-    $strReturn .= '<link href="'.CUtil::GetAdditionalFileURL("/bitrix/css/main/font-awesome.css").'" type="text/css" rel="stylesheet" />'."\n";
-}
-
-$strReturn .= '<div class="bx-breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">';
-
 $itemSize = count($arResult);
-for ($index = 0; $index < $itemSize; $index++)
+$strReturn = '
+    <div class="site-blocks-cover inner-page-cover overlay" style="background-image: url(' . SITE_TEMPLATE_PATH . '/images/hero_bg_2.jpg);" data-aos="fade" data-stellar-background-ratio="0.5">
+        <div class="container">
+            <div class="row align-items-center justify-content-center text-center">
+                <div class="col-md-10">
+                    <h1 class="mb-2">' . $APPLICATION->GetTitle() . '</h1>
+                        <div><a href="' . SITE_DIR . '">Главная страница</a>
+';
+for ($index = 1; $index < $itemSize; $index++)
 {
-	$title = htmlspecialcharsex($arResult[$index]["TITLE"]);
-	$arrow = ($index > 0 ? '<i class="fa fa-angle-right"></i>' : '');
-
-	if($arResult[$index]["LINK"] <> "" && $index != $itemSize - 1)
-	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item" id="bx_breadcrumb_'.$index.'" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
-				'.$arrow.'
-				<a href="'.$arResult[$index]["LINK"].'" title="'.$title.'" itemprop="item">
-					<span itemprop="name">'.$title.'</span>
-				</a>
-				<meta itemprop="position" content="'.($index + 1).'" />
-			</div>';
-	}
-	else
-	{
-		$strReturn .= '
-			<div class="bx-breadcrumb-item">
-				'.$arrow.'
-				<span>'.$title.'</span>
-			</div>';
-	}
+    if ($arResult[$index]["LINK"] == $arResult[--$index]["LINK"]) continue;
+    $title = htmlspecialcharsex($arResult[$index]["TITLE"]);
+    if (!$arResult[$index]["LINK"] && $arResult[$index]["LINK"] !== $APPLICATION->GetCurPage())
+    {
+        $strReturn .= '
+            <span class="mx-2 text-white">&bullet;</span>
+            <a href="' . $arResult[$index]["LINK"] . '"<strong class="text-white">' . $title . '</strong></a>
+        ';
+    }
+    elseif ($arResult[$index] == $arResult[count(--$arResult)])
+    {
+        $strReturn .= '
+            <span class="mx-2 text-white">&bullet;</span>
+            <strong class="text-white">' . $title . '</strong>
+        ';
+    }
 }
-
-$strReturn .= '<div style="clear:both"></div></div>';
-
+$strReturn .= '</div></div></div></div></div>';
 return $strReturn;
